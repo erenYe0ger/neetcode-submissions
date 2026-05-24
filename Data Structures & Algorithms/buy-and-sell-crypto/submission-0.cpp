e31faf315a2bbd6a0b2a5347d685d013893c153a
Goutam Mukherjee <quothyparser97@gmail.com>
2026-05-24 20:22:21 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int res = 0;
+        int buy = INT_MAX;
+        for(auto& p: prices) res = max(res, p-buy), buy = min(buy, p);
+        return res;
+    }
+};
