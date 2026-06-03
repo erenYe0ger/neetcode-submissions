e7ca77d472afba545a1dc7a50340670a0ba4af30
Goutam Mukherjee <quothyparser97@gmail.com>
2026-06-04 03:20:23 +0530

    Add: kth-largest-integer-in-a-stream - submission-1

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-1.cpp
@@ -0,0 +1,17 @@
+class KthLargest {
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int k;
+public:
+    KthLargest(int k, vector<int>& nums) : k(k) {
+        for(auto& i: nums) {
+            pq.push(i);
+            if(pq.size() > k) pq.pop();
+        }
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        if(pq.size() > k) pq.pop();
+        return pq.top();
+    }
+};
