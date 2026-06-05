d0ff22abc3ae9d886e880413f10fa7d2664aa91d
Goutam Mukherjee <quothyparser97@gmail.com>
2026-06-05 20:08:01 +0530

    Add: count-number-of-islands - submission-2

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-2.cpp
@@ -0,0 +1,46 @@
+class Solution {
+    typedef pair<int,int> P;
+    int dx[4] = {-1,0,1,0}, dy[4] = {0,-1,0,1};
+    int m, n;
+    void dfs(vector<vector<char>>& grid, int i, int j) {
+        grid[i][j] = '0';
+        for(int t=0; t<4; ++t) {
+            int x = i+dx[t], y = j+dy[t];
+            if(x >= m || x < 0 ||
+                y >= n || y < 0 || 
+                grid[x][y] == '0') continue;
+            dfs(grid, x, y);
+        }
+    }
+    void bfs(vector<vector<char>>& grid, int i, int j) {
+        queue<P> q;
+        q.push(make_pair(i, j));
+        grid[i][j] = '0';
+        while(!q.empty()) {
+            auto [r,c] = q.front(); q.pop();
+            for(int t=0; t<4; ++t) {
+                int x = r+dx[t], y = c+dy[t];
+                if(x >= m || x < 0 ||
+                    y >= n || y < 0 || 
+                    grid[x][y] == '0') continue;
+                grid[x][y] = '0';
+                q.push(make_pair(x, y));
+            }
+        }
+    }
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int cnt = 0;
+        m = grid.size(), n = grid[0].size();
+        for(int i=0; i<m; ++i) {
+            for(int j=0; j<n; ++j) {
+                if(grid[i][j] == '1') {
+                    ++cnt;
+                    // dfs(grid, i, j);
+                    bfs(grid, i, j);
+                }
+            }
+        }
+        return cnt;
+    }
+};
