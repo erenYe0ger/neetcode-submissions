9c70b9ae160fdb2351372615680d20f2cc952e98
Goutam Mukherjee <quothyparser97@gmail.com>
2026-06-05 23:42:53 +0530

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.cpp
@@ -0,0 +1,36 @@
+class DSU {
+    vector<int> size, par;
+public:
+    DSU(int n) {
+        size.assign(n,1);
+        par.resize(n);
+        for(int i=0; i<n; ++i) par[i] = i;
+    }
+    int find(int i) {
+        if(par[i] == i) return i;
+        return par[i] = find(par[i]);
+    }
+    void unite(int u, int v) {
+        u = find(u), v = find(v);
+        if(u == v) return;
+        if(size[u] < size[v]) swap(u,v);
+        size[u] += size[v];
+        par[v] = u;
+    }
+};
+
+class Solution {
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {
+        DSU dsu(n);
+        for(auto& edge: edges) {
+            int u = edge[0], v = edge[1];
+            dsu.unite(u,v);
+        }
+        unordered_set<int> st;
+        for(int i=0; i<n; ++i) {
+            st.insert(dsu.find(i));
+        }
+        return st.size();
+    }
+};
