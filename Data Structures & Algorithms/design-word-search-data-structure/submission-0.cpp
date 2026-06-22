3126d746a90a754703a5230e4ef1dd59493d5ed8
Goutam Mukherjee <quothyparser97@gmail.com>
2026-06-22 20:06:33 +0530

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.cpp
@@ -0,0 +1,44 @@
+class TrieNode {
+public:
+    bool endsHere = false;
+    TrieNode* children[26] = {nullptr};
+    ~TrieNode() {
+        for(int i=0; i<26; ++i) delete children[i];
+    }
+};
+
+class WordDictionary {
+    TrieNode* dummy = new TrieNode();
+    bool solve(string& word, int i, int n, TrieNode* &cur) {
+        if(i == n) return cur->endsHere;
+        char ch = word[i];
+        if(ch == '.') {
+            bool res = false;
+            for(int child=0; child<26; ++child) {
+                if(cur->children[child]) {
+                    res |= solve(word, i+1, n, cur->children[child]);
+                }
+            }
+            return res;
+        } 
+        else {
+            if(!cur->children[ch-'a']) return false;
+            return solve(word, i+1, n, cur->children[ch-'a']);
+        }
+    } 
+public:
+    void addWord(string word) {
+        TrieNode* cur = dummy;
+        for(auto& ch: word) {
+            if(!cur->children[ch-'a']) cur->children[ch-'a'] = new TrieNode();
+            cur = cur->children[ch-'a'];
+        }
+        cur->endsHere = true;
+    }
+    
+    bool search(string word) {
+        int n = word.size();
+        TrieNode* cur = dummy;
+        return solve(word, 0, n, cur);
+    }
+};
