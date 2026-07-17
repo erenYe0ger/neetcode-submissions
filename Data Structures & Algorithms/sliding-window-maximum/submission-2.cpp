1ddbec85e0ae2d0b6757becc62b9e2690c562c64
Goutam Mukherjee <quothyparser97@gmail.com>
2026-07-18 01:46:03 +0530

    Add: sliding-window-maximum - submission-2

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        deque<int> dq;
+        vector<int> res;
+        for(int i=0; i<n; ++i) {
+            while(!dq.empty() &&
+                 nums[i] > nums[dq.back()]) {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            if(i >= k-1) {
+                res.push_back(nums[dq.front()]);
+                if(dq.front() == i-k+1) {
+                    dq.pop_front();
+                }
+            }
+        }
+        return res;
+    }
+};
